9b8629b720a5cacec5db9cf0c9014f58125f1a85
Viraj Bhutada <virajnbhutada24@gmail.com>
2024-09-28 10:51:34 +0530

    Delete scripts directory

--- scripts/all_tracks.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-import pandas as pd
-import pickle
-
-music = pickle.load(open('music.pkl', 'rb'))
-musicdf = pd.DataFrame(music)
-st.title("All Tracks ")
-st.dataframe(musicdf, 3000, 500)
